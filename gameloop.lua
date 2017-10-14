47098892f6582bad4d66ab8480c1a2962f895aa7
Redless <brookmangabriel@gmail.com>
2017-10-14 11:45:28 -0500

    refactored code in preparation for menu system implementation

---
+++ gameloop.lua
@@ -0,0 +1,28 @@
+function updateInGame(dt)
+  mainDot.onLeftWall = false
+  mainDot.onRightWall = false
+  mainDot.onFloor = false
+  for i = 1,table.getn(currentBlocks) do
+    checkWalls(mainDot,currentBlocks[i])
+  end
+  applyVelocity(mainDot)
+  applyGravity(mainDot)
+  applyFriction(mainDot)
+  for i = 1,table.getn(currentBlocks) do
+    checkCollisions(mainDot,currentBlocks[i])
+  end
+  if (not mainDot.alreadyOnFloor) and (mainDot.onFloor) then
+    mainDot.xV = .6 * mainDot.direction
+    --love.audio.play(sfx)
+  end
+  mainDot.alreadyOnFloor = mainDot.onFloor
+end
+
+function drawInGame(dt)
+  love.graphics.setBackgroundColor(200, 200, 255)
+  love.graphics.scale(30, 30)
+  drawDot(mainDot,currentBlocks[1])
+  for i = 1,table.getn(currentBlocks) do
+    drawBlock(currentBlocks[i])
+  end
+end
--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
 require("block")
 require("dot")
+require("menu")
+require("gameloop")
 
 function love.load()
+  mode = "game"
   maxJump = 3
   maxSpeed = .2
   mainDot = dot(0,0,1,1)
@@ -12,51 +15,28 @@ function love.load()
 end
 
 function love.update(dt)
-  mainDot.onLeftWall = false
-  mainDot.onRightWall = false
-  mainDot.onFloor = false
-  for i = 1,table.getn(currentBlocks) do
-    checkWalls(mainDot,currentBlocks[i])
+  if mode == "game" then
+    updateInGame()
   end
-  applyVelocity(mainDot)
-  applyGravity(mainDot)
-  applyFriction(mainDot)
-  for i = 1,table.getn(currentBlocks) do
-    checkCollisions(mainDot,currentBlocks[i])
-  end
-  if (not mainDot.alreadyOnFloor) and (mainDot.onFloor) then
-    mainDot.xV = .6 * mainDot.direction
-    --love.audio.play(sfx)
-  end
-  mainDot.alreadyOnFloor = mainDot.onFloor
 end
 
 function love.draw(dt)
-  love.graphics.setBackgroundColor(200, 200, 255)
-  love.graphics.scale(30, 30)
-  drawDot(mainDot,currentBlocks[1])
-  for i = 1,table.getn(currentBlocks) do
-    drawBlock(currentBlocks[i])
+  if mode == "game" then
+    drawInGame()
   end
 end
 
 function love.keypressed(key)
-  if key == "w" then
-    mainDot.yV = mainDot.yV - .1
-  elseif key == "a" then
-    mainDot.xV = mainDot.xV - .1
-  elseif key == "s" then
-    mainDot.yV = mainDot.yV + .1
-  elseif key == "d" then
-    mainDot.xV = mainDot.xV + .1
-  elseif (key == "space") and mainDot.onFloor then
-    --love.audio.play(sfx)
-    jump(mainDot)
-  elseif (key == "space") and mainDot.onLeftWall then
-    leftWallKick(mainDot)
-  elseif (key == "space") and mainDot.onRightWall then
-    rightWallKick(mainDot)
-  elseif (key == "space") then
-    slash(mainDot)
+  if mode == "game" then
+    if (key == "space") and mainDot.onFloor then
+      --love.audio.play(sfx)
+      jump(mainDot)
+    elseif (key == "space") and mainDot.onLeftWall then
+      leftWallKick(mainDot)
+    elseif (key == "space") and mainDot.onRightWall then
+      rightWallKick(mainDot)
+    elseif (key == "space") then
+      slash(mainDot)
+    end
   end
 end
